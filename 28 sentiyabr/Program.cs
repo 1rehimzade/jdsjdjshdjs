c72e613731b52228cbd5c376ca3ed560907acc8c
nihatab104 <146129654+nihatab104@users.noreply.github.com>
2023-10-04 21:48:53 +0400

    first commit

---
+++ 28 sentiyabr/Program.cs
@@ -0,0 +1,17 @@
+﻿
+static void Main()
+{
+   
+    double[] ededler = { 1.5, 2.7, 3.1, 4.8, 5.2 };
+
+   
+    double cem = 0;
+
+    for (int i = 0; i < ededler.Length; i++)
+    {
+        cem += ededler[i];
+    }
+
+   
+    Console.WriteLine("Array elementlərinin cəmi: " + cem);
+}
